af263735b1b9f2abdafc987ee1d032128f30313f
simonayele <simonayele@gmail.com>
2019-09-27 03:04:50 +0000

    adding validation to grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
-  describe "gram#create action" do
+  describe "grams#create action" do
     it "should successfully create a new gram in our database" do
       post :create, params: { gram: { message: 'Hello!' } }
       expect(response).to redirect_to root_path
     
       gram = Gram.last
-    expect(gram.message).to eq("Hello!")
+      expect(gram.message).to eq("Hello!")
     end
 
       it "should properly deal with validation errors" do
