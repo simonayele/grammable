722b332fe84b034db76603ec9f7501338e55a5a3
simonayele <simonayele@gmail.com>
2019-10-04 00:46:41 +0000

    creating comments on grams

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jquery-rails'
 gem 'simple_form'
 gem 'popper_js', '~> 1.11.1'
 gem 'bootstrap', '4.0.0.alpha6'
+gem 'carrierwave', '~> 2.0'
+gem "figaro"
+gem "fog-aws"
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -37,6 +37,7 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def show
@@ -58,10 +59,8 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
   
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
+  
 end
